34e7b0a2dada80add8ef14c362f986f0ded73fae
Guilherme Muniz <gui.munizalemao@gmail.com>
2024-09-10 11:41:45 -0300

    instalando firebase no projeto

---
+++ miniblog/firebase/config.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAr-gAXB8No8oC4rg5iDbAL0q8dPYNDZd4",
+  authDomain: "miniblog-d2eed.firebaseapp.com",
+  projectId: "miniblog-d2eed",
+  storageBucket: "miniblog-d2eed.appspot.com",
+  messagingSenderId: "377220200381",
+  appId: "1:377220200381:web:381bcfa35dae55aeb944d4",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
