c3b9aa583da063a6ff81ed718e61c4f19c0b2e2a
Guilherme Muniz <gui.munizalemao@gmail.com>
2024-09-24 20:55:51 -0300

    validação de dados

--- miniblog/src/pages/CreatePost/CreatePost.jsx
+++ miniblog/src/pages/CreatePost/CreatePost.jsx
@@ -16,22 +16,38 @@ const CreatePost = () => {
 
   const { insertDocument, response } = useInsertDocument("posts");
 
+  const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("");
 
     // validate image URL
+    try {
+      new URL(image);
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.");
+    }
     // criar o array de tags
+
+    const tagsArray = tags.split(",").map((tag) => tag.trim().toLowerCase());
     //checar todos os valores
+    if (!title || !image || !tags || !body) {
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if (formError) return;
 
     insertDocument({
       title,
       image,
       body,
-      tags,
+      tagsArray,
       uid: user.uid,
       createBy: user.displayName,
     });
+
+    navigate("/");
   };
 
   return (
@@ -90,6 +106,7 @@ const CreatePost = () => {
           </button>
         )}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
