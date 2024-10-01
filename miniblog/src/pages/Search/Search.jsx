9610de3e936fe9908a53fca2376782f394e83257
Guilherme Muniz <gui.munizalemao@gmail.com>
2024-09-30 22:53:59 -0300

    função busca

---
+++ miniblog/src/pages/Search/Search.jsx
@@ -0,0 +1,41 @@
+import styles from "./Search.module.css";
+import { Link } from "react-router-dom";
+
+//hooks
+import { useFetchDocuments } from "../../hooks/useFetchDocument";
+import { useQuery } from "../../hooks/useQuery";
+
+//components
+import PostDetails from "../../components/PostDetails";
+
+const Search = () => {
+  const query = useQuery();
+
+  const search = query.get("q");
+
+  const {
+    documents: posts,
+    loading,
+    error,
+  } = useFetchDocuments("posts", search);
+
+  return (
+    <div className={styles.search_container}>
+      <h2>Search</h2>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts partir da sua busca</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts &&
+          posts.map((post) => <PostDetails key={post.id} post={post} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
